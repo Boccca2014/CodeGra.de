9bd4892ecbda7880c559d71960b97cdb67a88952
Thomas Schaper <thomas@codegra.de>
2020-08-20 14:28:44 +0200

    Improve error message when AutoTest is not yet available (#1418)
    Hopefully this makes it more clear that AutoTest will probably be available
    later on for students, and that this is not a bug.
    
    Co-authored-by: Olmo Kramer <olmo@codegrade.com>

--- psef/auth.py
+++ psef/auth.py
@@ -1483,7 +1483,10 @@ def ensure_any_of_permissions(
         'None of the permissions "{}" are enabled for user "{}"'.format(
             readable_join([p.name for p in permissions]),
             _get_cur_user().id,
-        ), APICodes.INCORRECT_PERMISSION, 403
+        ),
+        APICodes.INCORRECT_PERMISSION,
+        403,
+        missing_permissions=[permissions],
     )
 
 
@@ -1568,6 +1571,7 @@ def ensure_permission(  # pylint: disable=function-redefined
                 ),
                 APICodes.INCORRECT_PERMISSION,
                 403,
+                missing_permissions=[permission],
             )
     else:
         _raise_login_exception(
