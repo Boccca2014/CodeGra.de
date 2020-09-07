ab8a2a64a5f769a822a4003bea5e1f458937d582
Olmo Kramer <olmo@codegrade.com>
2020-09-07 12:18:19 +0200

    Fix assignment list item gear popover

--- src/components/Sidebar/AssignmentListItem.vue
+++ src/components/Sidebar/AssignmentListItem.vue
@@ -47,7 +47,7 @@
     </router-link>
     <router-link class="sidebar-item manage-link"
                  v-if="assignment.canManage && !small"
-                 v-b-popover="noPopover ? '' : 'Manage assignment'"
+                 v-b-popover.topleft.hover.window="noPopover ? '' : 'Manage assignment'"
                  :class="{ selected: manageSelected }"
                  :to="manageRoute(assignment)">
         <icon name="gear" />
