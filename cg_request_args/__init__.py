85c9e3040909c1868c6b4e75737d8f01d71ca3ac
Thomas Schaper <thomas@codegra.de>
2020-09-10 18:08:33 +0200

    Move structure to a more AST based form

--- cg_request_args/__init__.py
+++ cg_request_args/__init__.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
+import abc
 import enum
 import typing as t
 
-from typing_extensions import Literal, TypedDict
+from typing_extensions import Final, Literal, Protocol, TypedDict
 
 from cg_dt_utils import DatetimeWithTimezone
 
@@ -14,80 +15,122 @@ class _BaseDict(TypedDict):
 
 _BASE_DICT = t.TypeVar('_BASE_DICT', bound=_BaseDict)
 
-VALUE_LIKE = t.Union[str, int, float, DatetimeWithTimezone]
-
-ARG_TYPE = t.TypeVar(
-    'ARG_TYPE', bound='t.Union[_RequiredArgument, _OptionalArgument, VALUE_LIKE, ListOf]'
-)
-
 
 class MissingType(enum.Enum):
     token = 0
 
 
+_T = t.TypeVar('_T')
 MISSING: Literal[MissingType.token] = MissingType.token
 
 
-class ListOf(t.Generic[ARG_TYPE]):
-    def __init__(self, value: t.Type[ARG_TYPE]):
-        self._value = value
+class ParseError(ValueError):
+    pass
 
 
-class _RequiredArgument(t.Generic[_BASE_DICT]):
-    def __init__(
-        self,
-        key: str,
-        value: t.Type[ARG_TYPE],
-        doc: str,
-    ) -> None:
-        pass
+class _ParserLike(abc.ABC, t.Generic[_T]):
+    @abc.abstractmethod
+    def try_parse(self, value: object) -> _T:
+        ...
 
 
-class _OptionalArgument(t.Generic[_BASE_DICT]):
-    def __init__(
-            self,
-        key: str,
-        value: t.Type[ARG_TYPE],
-        doc: str,
-    ) -> None:
-        pass
+_VALUE_LIKE = t.TypeVar('_VALUE_LIKE', str, int, float, DatetimeWithTimezone)
 
 
-@t.overload
-def argument(
-    key: str,
-    value: t.Type[ARG_TYPE],
-    doc: str,
-    *,
-    optional: Literal[False] = False
-) -> _RequiredArgument[ARG_TYPE]:
-    ...
+class Value(t.Generic[_VALUE_LIKE], _ParserLike[_VALUE_LIKE]):
+    __slots__ = ('__typ', )
+
+    def __init__(self, typ: t.Type[_VALUE_LIKE]) -> None:
+        self.__typ: Final[t.Type[_VALUE_LIKE]] = typ
+
+    def try_parse(self, value: object) -> _VALUE_LIKE:
+        if isinstance(value, self.__typ):
+            return value
+        raise
 
 
-@t.overload
-def argument(
-    key: str, value: t.Type[ARG_TYPE], doc: str, *, optional: Literal[True]
-) -> _OptionalArgument[ARG_TYPE]:
-    if optional:
-        return _OptionalArgument(key, value, doc)
-    return _RequiredArgument(key, value, doc)
+
+class ListOf(t.Generic[_T], _ParserLike[t.List[_T]]):
+    __slots__ = ('__el_type', )
+
+    def __init__(self, el_type: _ParserLike[_T]):
+        self.__el_type: Final = el_type
+
+    def try_parse(self, value: object) -> t.List[_T]:
+        if not isinstance(value, list):
+            raise
+
+        el_type = self.__el_type
+        res = []
+        errors = []
+
+        for item in value:
+            try:
+                res.append(el_type.try_parse(item))
+            except ParseError as e:
+                errors.append(e)
+        if errors:
+            raise
+        else:
+            return res
 
 
-def argument(
-    key: str, value: t.Type[ARG_TYPE], doc: str, *, optional: bool = False
-) -> t.Union[_RequiredArgument[ARG_TYPE], _OptionalArgument[ARG_TYPE]]:
-    if optional:
-        return _OptionalArgument(key, value, doc)
-    return _RequiredArgument(key, value, doc)
+# class _RequiredArgument(t.Generic[_BASE_DICT]):
+#     def __init__(
+#         self,
+#         key: str,
+#         value: t.Type[ARG_TYPE],
+#         doc: str,
+#     ) -> None:
+#         pass
 
 
-class ArgumentParser(t.Generic[_BASE_DICT]):
-    @classmethod
-    def make_decorator(cls, name: str, *arguments: t.Iterable[ARG_TYPE])  -> t.Any:
-        return ArgumentParser()
+# class _OptionalArgument(t.Generic[_BASE_DICT]):
+#     def __init__(
+#         self,
+#         key: str,
+#         value: t.Type[ARG_TYPE],
+#         doc: str,
+#     ) -> None:
+#         pass
+
+
+# @t.overload
+# def argument(
+#     key: str,
+#     value: t.Type[ARG_TYPE],
+#     doc: str,
+#     *,
+#     optional: Literal[False] = False
+# ) -> _RequiredArgument[ARG_TYPE]:
+#     ...
+
+
+# @t.overload
+# def argument(
+#     key: str, value: t.Type[ARG_TYPE], doc: str, *, optional: Literal[True]
+# ) -> _OptionalArgument[ARG_TYPE]:
+#     if optional:
+#         return _OptionalArgument(key, value, doc)
+#     return _RequiredArgument(key, value, doc)
+
+
+# def argument(
+#     key: str, value: t.Type[ARG_TYPE], doc: str, *, optional: bool = False
+# ) -> t.Union[_RequiredArgument[ARG_TYPE], _OptionalArgument[ARG_TYPE]]:
+#     if optional:
+#         return _OptionalArgument(key, value, doc)
+#     return _RequiredArgument(key, value, doc)
+
+
+# class _DictParser(t.Generic[_BASE_DICT]):
+#     def add_option(cls, name: str, *arguments: t.Iterable[ARG_TYPE]) -> t.Any:
+#         return ArgumentParser()
 
 
 if t.TYPE_CHECKING:
-    @ArgumentParser.make_decorator('parser', argument('id', int, 'some doc'))
-    def tester(parser: object) -> t.Any:
-        pass
+    reveal_type(ListOf(Value(int)))
+    list_of_ints1 = ListOf(Value(int)).try_parse('[asdfasdf]')
+    list_of_ints2 = ListOf(Value(int)).try_parse([1, 2])
+    reveal_type(list_of_ints1)
+    reveal_type(list_of_ints2)
