7ef08e1443451dc536912d7c5758aecc50d837c5
Thomas Schaper <thomas@codegra.de>
2020-09-11 01:35:22 +0200

    Add working initial framework

--- cg_request_args/__init__.py
+++ cg_request_args/__init__.py
@@ -21,6 +21,10 @@ class MissingType(enum.Enum):
 
 
 _T = t.TypeVar('_T')
+_T_COV = t.TypeVar('_T_COV', covariant=True)
+_Y = t.TypeVar('_Y')
+_Z = t.TypeVar('_Z')
+_X = t.TypeVar('_X')
 MISSING: Literal[MissingType.token] = MissingType.token
 
 
@@ -28,29 +32,96 @@ class ParseError(ValueError):
     pass
 
 
-class _ParserLike(abc.ABC, t.Generic[_T]):
+class _ParserLike(t.Generic[_T_COV]):
     @abc.abstractmethod
-    def try_parse(self, value: object) -> _T:
+    def try_parse(self, value: object) -> _T_COV:
         ...
 
+    def __or__(self: _ParserLike[_T],
+               other: _ParserLike[_Y]) -> _ParserLike[t.Union[_T, _Y]]:
+        if (isinstance(self,
+                       _SimpleValue)) and isinstance(other, _SimpleValue):
+            return _SimpleUnion(self.typ, other.typ)
+        if (isinstance(self,
+                       _SimpleUnion)) and isinstance(other, _SimpleValue):
+            return _SimpleUnion(*self.typs, other.typ)
+        if (isinstance(self,
+                       _SimpleValue)) and isinstance(other, _SimpleUnion):
+            return _SimpleUnion(self.typ, *other.typs)
+        if (isinstance(self,
+                       _SimpleUnion)) and isinstance(other, _SimpleUnion):
+            return _SimpleUnion(*self.typs, *other.typs)
+        return _Union(self, other)
 
-_VALUE_LIKE = t.TypeVar('_VALUE_LIKE', str, int, float, DatetimeWithTimezone)
+
+_SIMPLE_VALUE = t.TypeVar('_SIMPLE_VALUE', str, int, float, bool, None)
 
 
-class Value(t.Generic[_VALUE_LIKE], _ParserLike[_VALUE_LIKE]):
-    __slots__ = ('__typ', )
+class _SimpleValue(t.Generic[_SIMPLE_VALUE], _ParserLike[_SIMPLE_VALUE]):
+    __slots__ = ('typ', )
 
-    def __init__(self, typ: t.Type[_VALUE_LIKE]) -> None:
-        self.__typ: Final[t.Type[_VALUE_LIKE]] = typ
+    def __init__(self, typ: t.Type[_SIMPLE_VALUE]) -> None:
+        self.typ: Final[t.Type[_SIMPLE_VALUE]] = typ
 
-    def try_parse(self, value: object) -> _VALUE_LIKE:
-        if isinstance(value, self.__typ):
+    def try_parse(self, value: object) -> _SIMPLE_VALUE:
+        if isinstance(value, self.typ):
             return value
         raise
 
 
+_SIMPLE_UNION = t.TypeVar(
+    '_SIMPLE_UNION', bound=t.Union[str, int, float, bool, None]
+)
 
-class ListOf(t.Generic[_T], _ParserLike[t.List[_T]]):
+
+class _SimpleUnion(t.Generic[_SIMPLE_UNION], _ParserLike[_SIMPLE_UNION]):
+    __slots__ = ('typs', )
+
+    def __init__(self, *typs: t.Type[_SIMPLE_UNION]) -> None:
+        self.typs: Final[t.Tuple[t.Type[_SIMPLE_UNION], ...]] = typs
+
+    def try_parse(self, union: object) -> _SIMPLE_UNION:
+        if isinstance(union, self.typs):
+            return union  # type: ignore
+        raise
+
+
+class _RichValue(t.Generic[_SIMPLE_VALUE, _T], _ParserLike[_T]):
+    __slots__ = ('__value', '__converter')
+
+    def __init__(
+        self, typ: t.Type[_SIMPLE_VALUE],
+        converter: t.Callable[[_SIMPLE_VALUE], _T]
+    ) -> None:
+        self.__value: Final[_SimpleValue[_SIMPLE_VALUE]] = _SimpleValue(typ)
+        self.__converter: Final[t.Callable[[_SIMPLE_VALUE], _T]] = converter
+
+    def try_parse(self, value: object) -> _T:
+        res = self.__value.try_parse(value)
+        # TODO: Error handling
+        return self.__converter(res)
+
+
+class _Union(t.Generic[_T, _Y], _ParserLike[t.Union[_T, _Y]]):
+    __slots__ = ('__first', '__second')
+
+    def __init__(
+        self, first: _ParserLike[_T], second: _ParserLike[_Y]
+    ) -> None:
+        self.__first = first
+        self.__second = second
+
+    def try_parse(self, value: object) -> t.Union[_T, _Y]:
+        try:
+            return self.__first.try_parse(value)
+        except ParseError as first_err:
+            try:
+                return self.__second.try_parse(value)
+            except ParseError as second_err:
+                raise
+
+
+class _List(t.Generic[_T], _ParserLike[t.List[_T]]):
     __slots__ = ('__el_type', )
 
     def __init__(self, el_type: _ParserLike[_T]):
@@ -75,62 +146,82 @@ class ListOf(t.Generic[_T], _ParserLike[t.List[_T]]):
             return res
 
 
-# class _RequiredArgument(t.Generic[_BASE_DICT]):
-#     def __init__(
-#         self,
-#         key: str,
-#         value: t.Type[ARG_TYPE],
-#         doc: str,
-#     ) -> None:
-#         pass
+_Key = t.TypeVar('_Key', bound=str)
 
 
-# class _OptionalArgument(t.Generic[_BASE_DICT]):
-#     def __init__(
-#         self,
-#         key: str,
-#         value: t.Type[ARG_TYPE],
-#         doc: str,
-#     ) -> None:
-#         pass
+class _Argument(t.Generic[_T, _Key], _ParserLike[_T]):
+    __slots__ = ('key', '__value', '__doc')
+
+    def __init__(
+        self,
+        key: _Key,
+        value: _ParserLike[_T],
+        doc: str,
+    ) -> None:
+        self.key: Final = key
+        self.__value = value
+        self.__doc = doc
+
+    def try_parse(self, value: object) -> _T:
+        return self.__value.try_parse(value)
 
 
-# @t.overload
-# def argument(
-#     key: str,
-#     value: t.Type[ARG_TYPE],
-#     doc: str,
-#     *,
-#     optional: Literal[False] = False
-# ) -> _RequiredArgument[ARG_TYPE]:
-#     ...
+class _RequiredArgument(t.Generic[_T, _Key], _Argument[_T, _Key]):
+    pass
 
 
-# @t.overload
-# def argument(
-#     key: str, value: t.Type[ARG_TYPE], doc: str, *, optional: Literal[True]
-# ) -> _OptionalArgument[ARG_TYPE]:
-#     if optional:
-#         return _OptionalArgument(key, value, doc)
-#     return _RequiredArgument(key, value, doc)
+class _OptionalArgument(t.Generic[_T, _Key], _Argument[_T, _Key]):
+    pass
 
 
-# def argument(
-#     key: str, value: t.Type[ARG_TYPE], doc: str, *, optional: bool = False
-# ) -> t.Union[_RequiredArgument[ARG_TYPE], _OptionalArgument[ARG_TYPE]]:
-#     if optional:
-#         return _OptionalArgument(key, value, doc)
-#     return _RequiredArgument(key, value, doc)
+class _FixedMapping(t.Generic[_BASE_DICT], _ParserLike[_BASE_DICT]):
+    __slots__ = ('__arguments', )
+
+    def __init__(self, *arguments: object) -> None:
+        self.__arguments = t.cast(t.Sequence[_Argument], arguments)
+
+    def try_parse(self, value: object) -> _BASE_DICT:
+        if not isinstance(value, dict):
+            raise
+
+        result = {}
+        errors = []
+        for arg in self.__arguments:
+            if arg.key not in value:
+                if isinstance(arg, _OptionalArgument):
+                    result[arg.key] = MISSING
+                else:
+                    errors.append(ParseError())
+                continue
+
+            try:
+                result[arg.key] = arg.try_parse(value[arg.key])
+            except ParseError as exc:
+                errors.append(exc)
+
+        if errors:
+            raise
+
+        return t.cast(_BASE_DICT, result)
 
 
-# class _DictParser(t.Generic[_BASE_DICT]):
-#     def add_option(cls, name: str, *arguments: t.Iterable[ARG_TYPE]) -> t.Any:
-#         return ArgumentParser()
+if __name__ == '__main__':
+    mapping = _FixedMapping(
+        _RequiredArgument(
+            'a',
+            _Union(
+                _SimpleValue(str),
+                _RichValue(str, DatetimeWithTimezone.fromisoformat),
+            ),
+            'hello',
+        ),
+        _OptionalArgument(
+            'b',
+            _List(_SimpleValue(int)),
+            'hello',
+        ),
+    )
 
+    print(_SimpleValue(int) | _SimpleValue(str))
 
-if t.TYPE_CHECKING:
-    reveal_type(ListOf(Value(int)))
-    list_of_ints1 = ListOf(Value(int)).try_parse('[asdfasdf]')
-    list_of_ints2 = ListOf(Value(int)).try_parse([1, 2])
-    reveal_type(list_of_ints1)
-    reveal_type(list_of_ints2)
+    print(mapping.try_parse({'a': 'd', 'b': [10, 100]}))
--- cg_request_args/plugin.py
+++ cg_request_args/plugin.py
@@ -11,91 +11,197 @@ from collections import OrderedDict
 # disabling this pylint error, but haven't found an explanation why...
 from mypy.nodes import TypeInfo  # pylint: disable=no-name-in-module
 from mypy.types import (  # pylint: disable=no-name-in-module
-    Type, Instance, TypeType, UnionType, LiteralType, CallableType,
-    TypedDictType
+    Type, AnyType, Instance, TypeType, TupleType, UnionType, LiteralType,
+    CallableType, TypedDictType
 )
 from mypy.plugin import (  # pylint: disable=no-name-in-module
-    Plugin, MethodContext
+    Plugin, MethodContext, FunctionContext
 )
 
-from cg_request_args import MISSING
+from cg_request_args import MissingType
 
 
-def add_argument_callback(ctx: MethodContext) -> Type:
-    breakpoint()
-    typeddict = ctx.type.args[0]
-    assert isinstance(typeddict, TypedDictType)
-    existing_items = typeddict.items
+def argument_callback(ctx: FunctionContext) -> Type:
+    key = ctx.arg_types[0][0]
+    if not isinstance(key, Instance):
+        return ctx.default_return_type
 
-    (key, ), (value, ), doc, *optional = ctx.arg_types
-    assert isinstance(key, Instance)
-    assert isinstance(key.last_known_value, LiteralType)
-    key_value = key.last_known_value.value
-    if key_value in existing_items:
+    if not isinstance(key.last_known_value, LiteralType):
         ctx.api.fail(
-            'This Arguments already has a key named {}'.format(key_value),
-            ctx.context
+            'The key to the _Argument constructor should be a literal',
+            ctx.context,
         )
-        return ctx.type
+        return ctx.default_return_type
 
-    new_type = value.items()[0].ret_type
-    if optional and optional[0] and optional[0][0].value:
-        new_type = UnionType(
-            items=[
-                new_type,
-                LiteralType(
-                    value='MISSING',
-                    fallback=ctx.api.named_generic_type(
-                        'cg_request_args.MissingType',
-                        [],
+    # if isinstance(ctx.default_return_type.args[0], AnyType):
+    #     assert False
+    # For some reason mypy doesn't pick up the first argument correctly when
+    # using a pattern like: `_FixedMapping(_RequiredArgument(...))` so we
+    # simply add the argument back in here.
+    assert isinstance(ctx.default_return_type, Instance)
+    return ctx.default_return_type.copy_modified(
+        args=[ctx.default_return_type.args[0], key.last_known_value]
+    )
+
+
+def fixed_mapping_callback(ctx: FunctionContext) -> Type:
+    fallback = ctx.api.named_generic_type('typing_extensions._TypedDict', [])
+
+    required_keys = set()
+    items = OrderedDict()
+
+    for idx, arg in enumerate(ctx.arg_types[0]):
+        required = False
+
+        if isinstance(arg, AnyType):
+            ctx.api.fail((
+                'Argument number {} was an "Any" which is not allowed as an'
+                ' argument to _FixedMapping'
+            ).format(idx + 1), ctx.context)
+            continue
+        try:
+            assert isinstance(arg, Instance)
+            typ = arg.type.fullname
+        except:
+            typ = '????'
+
+        if typ == 'cg_request_args._RequiredArgument':
+            required = True
+        elif typ != 'cg_request_args._OptionalArgument':
+            ctx.api.fail((
+                'Argument number {} provided was of wrong type, expected'
+                ' cg_request_args._RequiredArgument or'
+                ' cg_request_args._OptionalArgument, but got {}.'
+            ).format(idx + 1, typ), ctx.context)
+            continue
+
+        assert isinstance(arg, Instance)
+        key_typevar = arg.args[1]
+        if not isinstance(key_typevar, LiteralType):
+            ctx.api.fail((
+                'Second parameter of the argument should be a literal, this'
+                ' was not the case for argument {}'
+            ).format(idx + 1), ctx.context)
+            continue
+
+        key = key_typevar.value
+        if not isinstance(key, str):
+            ctx.api.fail((
+                'Key should be of type string, but was of type {} for argument'
+                ' number {}.'
+            ).format(type(key), idx + 1), ctx.context)
+            continue
+
+        if key in items:
+            ctx.api.fail((
+                'Key {} was already present, but given again as argument {}.'
+            ).format(key, idx + 1), ctx.context)
+            continue
+
+        required_keys.add(key)
+
+        value_type = arg.args[0]
+        if not required:
+            # Use literal here
+            value_type = UnionType(
+                items=[
+                    value_type,
+                    LiteralType(
+                        MissingType.token.name,
+                        ctx.api.named_type('cg_request_args.MissingType'),
                     ),
-                )
-            ]
-        )
-
-    return ctx.type.copy_modified(
-        args=[
-            TypedDictType(
-                items=OrderedDict(
-                    itertools.chain(
-                        existing_items.items(),
-                        [(key_value, new_type)],
-                    )
-                ),
-                required_keys=set(
-                    itertools.chain(existing_items.keys(), [key_value])
-                ),
-                line=typeddict.line,
-                fallback=typeddict.fallback,
+                ]
             )
+
+        items[key] = value_type
+
+    assert isinstance(ctx.default_return_type, Instance)
+    return ctx.default_return_type.copy_modified(
+        args=[
+            TypedDictType(OrderedDict(items), required_keys, fallback),
         ]
     )
-    # existing_items[key_value]
+
+
+# def add_argument_callback(ctx: MethodContext) -> Type:
+#     breakpoint()
+#     typeddict = ctx.type.args[0]
+#     assert isinstance(typeddict, TypedDictType)
+#     existing_items = typeddict.items
+
+#     (key, ), (value, ), doc, *optional = ctx.arg_types
+#     assert isinstance(key, Instance)
+#     assert isinstance(key.last_known_value, LiteralType)
+#     key_value = key.last_known_value.value
+#     if key_value in existing_items:
+#         ctx.api.fail(
+#             'This Arguments already has a key named {}'.format(key_value),
+#             ctx.context
+#         )
+#         return ctx.type
+
+#     new_type = value.items()[0].ret_type
+#     if optional and optional[0] and optional[0][0].value:
+#         new_type = UnionType(
+#             items=[
+#                 new_type,
+#                 LiteralType(
+#                     value='MISSING',
+#                     fallback=ctx.api.named_generic_type(
+#                         'cg_request_args.MissingType',
+#                         [],
+#                     ),
+#                 )
+#             ]
+#         )
+
+#     return ctx.type.copy_modified(
+#         args=[
+#             TypedDictType(
+#                 items=OrderedDict(
+#                     itertools.chain(
+#                         existing_items.items(),
+#                         [(key_value, new_type)],
+#                     )
+#                 ),
+#                 required_keys=set(
+#                     itertools.chain(existing_items.keys(), [key_value])
+#                 ),
+#                 line=typeddict.line,
+#                 fallback=typeddict.fallback,
+#             )
+#         ]
+#     )
+# existing_items[key_value]
 
 
 class CgRequestArgPlugin(Plugin):
     """Mypy plugin definition.
     """
 
-    def get_method_hook(  # pylint: disable=no-self-use
+    # def get_method_hook(  # pylint: disable=no-self-use
+    #     self,
+    #     fullname: str,
+    # ) -> t.Optional[t.Callable[[MethodContext], Type]]:
+    #     """Get the function to be called by mypy.
+    #     """
+    #     if fullname == 'cg_request_args.ArgumentParser.make_decorator':
+    #         return add_argument_callback
+
+    #     return None
+
+    def get_function_hook(  # pylint: disable=no-self-use
         self,
         fullname: str,
-    ) -> t.Optional[t.Callable[[MethodContext], Type]]:
+    ) -> t.Optional[t.Callable[[FunctionContext], Type]]:
         """Get the function to be called by mypy.
         """
-        if fullname == 'cg_request_args.ArgumentParser.make_decorator':
-            return add_argument_callback
-
-        return None
-
-    def get_method_hook(  # pylint: disable=no-self-use
-        self,
-        fullname: str,
-    ) -> t.Optional[t.Callable[[MethodContext], Type]]:
-        """Get the function to be called by mypy.
-        """
-        if fullname == 'cg_request_args.ArgumentParser.make_decorator':
-            return add_argument_callback
+        if fullname == 'cg_request_args._RequiredArgument':
+            return argument_callback
+        if fullname == 'cg_request_args._OptionalArgument':
+            return argument_callback
+        if fullname == 'cg_request_args._FixedMapping':
+            return fixed_mapping_callback
 
         return None
 
