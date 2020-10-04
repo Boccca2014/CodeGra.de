42b51ce4b665f1e5f55c98e15e924a8ebeb9d24f
Thomas Schaper <thomas@codegra.de>
2020-10-04 22:29:19 +0200

    Fix wrong change

--- psef/auth.py
+++ psef/auth.py
@@ -2086,8 +2086,7 @@ class _FlaskOAuthValidator(RequestValidatorMixin):
         '''
         # base_url is used because of:
         # https://github.com/instructure/canvas-lms/issues/600
-        base_url = 'https://dev-thomas.codegra.de/api/v1/lti/launch/1'
-        return (req.method, base_url, dict(req.headers), req.form.copy())
+        return (req.method, req.base_url, dict(req.headers), req.form.copy())
 
 
 def ensure_valid_oauth(
