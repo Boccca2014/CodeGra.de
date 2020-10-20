394a22a9ca3277da142ffc8ad7ea66071d7d6f38
Thomas Schaper <thomas@codegra.de>
2020-10-20 10:39:29 +0200

    Add merge migration

---
+++ migrations/versions/639a9ade56fc_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 639a9ade56fc
+Revises: 0d249267d800, 93b7165d8d9f
+Create Date: 2020-10-20 10:39:20.374052
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '639a9ade56fc'
+down_revision = ('0d249267d800', '93b7165d8d9f')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
